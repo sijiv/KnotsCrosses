cc02ff7e441fd63c6cb8886a546c175d427d4b99
Siji Varghese <siji_varghese@hotmail.com>
2014-10-21 10:31:41 -0400

    Spell Rorrections.

--- My.SignalRTest/Controllers/HomeController.cs
+++ My.SignalRTest/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace My.SignalRTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "SignalR with ASP.Net MVC";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Siji's contact page.";
 
             return View();
         }
